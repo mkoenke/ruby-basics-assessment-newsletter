5c1732e952b9c78c2d75315c22201077ce1ee016
mkoenke <mkoenke@sas.upenn.edu>
2020-10-18 14:03:45 -0400

    completed lab

--- newsletter.rb
+++ newsletter.rb
@@ -30,6 +30,9 @@ ARTICLES = [
 def calculate_recipients
   # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
   # write a method that will return an array of only the subscribers who haven't unsubscribed
+  SUBSCRIBERS.reject do |email|
+    UNSUBSCRIBED.include?(email)
+  end
 end
 
 def first_n_articles(number_of_articles
@@ -40,17 +43,25 @@ def print_recipients
   # Write a method that uses the output of calculate_recipients
   # and returns a list of emails separated by commas
   # Ex) "abc@email.com, def@email.com, ghi@email.com"
+  puts calculate_recipients.join(", ")
 end
 
 def print_one_article(article)
   # Write a method that will take an article hash
   # and print the title, author and text as a formatted string
   # See the README/sample output for examples
+  puts "#{article[:title]}"
+  puts "by: #{article[:author]}"
+  puts "#{article[:text]}"
+  puts ""
 end
 
 def print_many_articles(articles)
   # Write a method that will take in an array of article hashes
   # and format each one using the print_one_article method
+  articles.each do |article|
+    print_one_article(article)
+  end
 end
 
 def format_campus_location(campus)
